aad1d926ee2b03752da0039c8cb2c9c74b792a04
Alok Singh <alokbeniwal@gmail.com>
2020-11-11 21:59:33 -0800

    fmt

--- deepul/hw4_helper.py
+++ deepul/hw4_helper.py
@@ -10,7 +10,18 @@ import scipy.ndimage
 import cv2
 import deepul.pytorch_util as ptu
 
-CLASSES = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+CLASSES = (
+    "plane",
+    "car",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+)
 
 import numpy as np
 import math
@@ -20,24 +31,26 @@ softmax = None
 model = None
 device = torch.device("cuda:0")
 
+
 def plot_gan_training(losses, title, fname):
     plt.figure()
     n_itr = len(losses)
     xs = np.arange(n_itr)
 
-    plt.plot(xs, losses, label='loss')
+    plt.plot(xs, losses, label="loss")
     plt.legend()
     plt.title(title)
-    plt.xlabel('Training Iteration')
-    plt.ylabel('Loss')
+    plt.xlabel("Training Iteration")
+    plt.ylabel("Loss")
     savefig(fname)
+
 
 def q1_gan_plot(data, samples, xs, ys, title, fname):
     plt.figure()
-    plt.hist(samples, bins=50, density=True, alpha=0.7, label='fake')
-    plt.hist(data, bins=50, density=True, alpha=0.7, label='real')
+    plt.hist(samples, bins=50, density=True, alpha=0.7, label="fake")
+    plt.hist(data, bins=50, density=True, alpha=0.7, label="real")
 
-    plt.plot(xs, ys, label='discrim')
+    plt.plot(xs, ys, label="discrim")
     plt.legend()
     plt.title(title)
     savefig(fname)
@@ -47,17 +60,19 @@ def q1_gan_plot(data, samples, xs, ys, title, fname):
 ##### Question 1 #####
 ######################
 
+
 def q1_data(n=20000):
     assert n % 2 == 0
-    gaussian1 = np.random.normal(loc=-1, scale=0.25, size=(n//2,))
-    gaussian2 = np.random.normal(loc=0.5, scale=0.5, size=(n//2,))
+    gaussian1 = np.random.normal(loc=-1, scale=0.25, size=(n // 2,))
+    gaussian2 = np.random.normal(loc=0.5, scale=0.5, size=(n // 2,))
     data = (np.concatenate([gaussian1, gaussian2]) + 1).reshape([-1, 1])
     scaled_data = (data - np.min(data)) / (np.max(data) - np.min(data) + 1e-8)
-    return 2 * scaled_data -1
+    return 2 * scaled_data - 1
+
 
 def visualize_q1_dataset():
     data = q1_data()
-    plt.hist(data, bins=50, alpha=0.7, label='train data')
+    plt.hist(data, bins=50, alpha=0.7, label="train data")
     plt.legend()
     plt.show()
 
@@ -67,19 +82,37 @@ def q1_save_results(part, fn):
     losses, samples1, xs1, ys1, samples_end, xs_end, ys_end = fn(data)
 
     # loss plot
-    plot_gan_training(losses, 'Q1{} Losses'.format(part), 'results/q1{}_losses.png'.format(part))
+    plot_gan_training(
+        losses, "Q1{} Losses".format(part), "results/q1{}_losses.png".format(part)
+    )
 
     # samples
-    q1_gan_plot(data, samples1, xs1, ys1, 'Q1{} Epoch 1'.format(part), 'results/q1{}_epoch1.png'.format(part))
-    q1_gan_plot(data, samples_end, xs_end, ys_end, 'Q1{} Final'.format(part), 'results/q1{}_final.png'.format(part))
+    q1_gan_plot(
+        data,
+        samples1,
+        xs1,
+        ys1,
+        "Q1{} Epoch 1".format(part),
+        "results/q1{}_epoch1.png".format(part),
+    )
+    q1_gan_plot(
+        data,
+        samples_end,
+        xs_end,
+        ys_end,
+        "Q1{} Final".format(part),
+        "results/q1{}_final.png".format(part),
+    )
+
 
 ######################
 ##### Question 2 #####
 ######################
 
+
 def calculate_is(samples):
-    assert (type(samples[0]) == np.ndarray)
-    assert (len(samples[0].shape) == 3)
+    assert type(samples[0]) == np.ndarray
+    assert len(samples[0].shape) == 3
 
     model = GoogLeNet().to(ptu.device)
     model.load_state_dict(torch.load("deepul/deepul/hw4_utils/classifier.pt"))
@@ -93,7 +126,7 @@ def calculate_is(samples):
         for i in range(n_batches):
             sys.stdout.write(".")
             sys.stdout.flush()
-            inp = ptu.FloatTensor(samples[(i * bs):min((i + 1) * bs, len(samples))])
+            inp = ptu.FloatTensor(samples[(i * bs) : min((i + 1) * bs, len(samples))])
             pred = ptu.get_numpy(softmax(inp))
             preds.append(pred)
     preds = np.concatenate(preds, 0)
@@ -101,15 +134,19 @@ def calculate_is(samples):
     kl = np.mean(np.sum(kl, 1))
     return np.exp(kl)
 
+
 def load_q2_data():
-    train_data = torchvision.datasets.CIFAR10("./data", transform=torchvision.transforms.ToTensor(),
-                                              download=True, train=True)
+    train_data = torchvision.datasets.CIFAR10(
+        "./data", transform=torchvision.transforms.ToTensor(), download=True, train=True
+    )
     return train_data
+
 
 def visualize_q2_data():
     train_data = load_q2_data()
     imgs = train_data.data[:100]
-    show_samples(imgs, title=f'CIFAR-10 Samples')
+    show_samples(imgs, title=f"CIFAR-10 Samples")
+
 
 def q2_save_results(fn):
     train_data = load_q2_data()
@@ -117,65 +154,95 @@ def q2_save_results(fn):
     train_losses, samples = fn(train_data)
 
     print("Inception score:", calculate_is(samples.transpose([0, 3, 1, 2])))
-    plot_gan_training(train_losses, 'Q2 Losses', 'results/q2_losses.png')
-    show_samples(samples[:100] * 255.0, fname='results/q2_samples.png', title=f'CIFAR-10 generated samples')
+    plot_gan_training(train_losses, "Q2 Losses", "results/q2_losses.png")
+    show_samples(
+        samples[:100] * 255.0,
+        fname="results/q2_samples.png",
+        title=f"CIFAR-10 generated samples",
+    )
+
 
 ######################
 ##### Question 3 #####
 ######################
 
+
 def load_q3_data():
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-    train_data = torchvision.datasets.MNIST(root="./data", train=True, download=True, transform=transform)
-    test_data = torchvision.datasets.MNIST(root="./data", train=False, download=True, transform=transform)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))]
+    )
+    train_data = torchvision.datasets.MNIST(
+        root="./data", train=True, download=True, transform=transform
+    )
+    test_data = torchvision.datasets.MNIST(
+        root="./data", train=False, download=True, transform=transform
+    )
     return train_data, test_data
+
 
 def visualize_q3_data():
     train_data, _ = load_q3_data()
     imgs = train_data.data[:100]
-    show_samples(imgs.reshape([100, 28, 28, 1]) * 255.0, title=f'MNIST samples')
+    show_samples(imgs.reshape([100, 28, 28, 1]) * 255.0, title=f"MNIST samples")
+
 
 def plot_q3_supervised(pretrained_losses, random_losses, title, fname):
     plt.figure()
     xs = np.arange(len(pretrained_losses))
-    plt.plot(xs, pretrained_losses, label='bigan')
+    plt.plot(xs, pretrained_losses, label="bigan")
     xs = np.arange(len(random_losses))
-    plt.plot(xs, random_losses, label='random init')
+    plt.plot(xs, random_losses, label="random init")
     plt.legend()
     plt.title(title)
     savefig(fname)
 
+
 def q3_save_results(fn):
     train_data, test_data = load_q3_data()
-    gan_losses, samples, reconstructions, pretrained_losses, random_losses = fn(train_data, test_data)
+    gan_losses, samples, reconstructions, pretrained_losses, random_losses = fn(
+        train_data, test_data
+    )
 
-    plot_gan_training(gan_losses, 'Q3 Losses', 'results/q3_gan_losses.png')
-    plot_q3_supervised(pretrained_losses, random_losses, 'Linear classification losses', 'results/q3_supervised_losses.png')
-    show_samples(samples * 255.0, fname='results/q3_samples.png', title='BiGAN generated samples')
-    show_samples(reconstructions * 255.0, nrow=20, fname='results/q3_reconstructions.png', title=f'BiGAN reconstructions')
-    print('BiGAN final linear classification loss:', pretrained_losses[-1])
-    print('Random encoder linear classification loss:', random_losses[-1])
+    plot_gan_training(gan_losses, "Q3 Losses", "results/q3_gan_losses.png")
+    plot_q3_supervised(
+        pretrained_losses,
+        random_losses,
+        "Linear classification losses",
+        "results/q3_supervised_losses.png",
+    )
+    show_samples(
+        samples * 255.0, fname="results/q3_samples.png", title="BiGAN generated samples"
+    )
+    show_samples(
+        reconstructions * 255.0,
+        nrow=20,
+        fname="results/q3_reconstructions.png",
+        title=f"BiGAN reconstructions",
+    )
+    print("BiGAN final linear classification loss:", pretrained_losses[-1])
+    print("Random encoder linear classification loss:", random_losses[-1])
+
 
 ######################
 ##### Question 4 #####
 ######################
 
+
 def get_colored_mnist(data):
     # from https://www.wouterbulten.nl/blog/tech/getting-started-with-gans-2-colorful-mnist/
     # Read Lena image
-    lena = PILImage.open('deepul/deepul/hw4_utils/lena.jpg')
+    lena = PILImage.open("deepul/deepul/hw4_utils/lena.jpg")
 
     # Resize
-    batch_resized = np.asarray([scipy.ndimage.zoom(image, (2.3, 2.3, 1), order=1) for image in data])
+    batch_resized = np.asarray(
+        [scipy.ndimage.zoom(image, (2.3, 2.3, 1), order=1) for image in data]
+    )
 
     # Extend to RGB
     batch_rgb = np.concatenate([batch_resized, batch_resized, batch_resized], axis=3)
 
     # Make binary
-    batch_binary = (batch_rgb > 0.5)
+    batch_binary = batch_rgb > 0.5
 
     batch = np.zeros((data.shape[0], 28, 28, 3))
 
@@ -189,8 +256,11 @@ def get_colored_mnist(data):
         # Invert the colors at the location of the number
         image[batch_binary[i]] = 1 - image[batch_binary[i]]
 
-        batch[i] = cv2.resize(image, (0, 0), fx=28 / 64, fy=28 / 64, interpolation=cv2.INTER_AREA)
+        batch[i] = cv2.resize(
+            image, (0, 0), fx=28 / 64, fy=28 / 64, interpolation=cv2.INTER_AREA
+        )
     return batch.transpose(0, 3, 1, 2)
+
 
 def load_q4_data():
     train, _ = load_q3_data()
@@ -198,11 +268,15 @@ def load_q4_data():
     colored_mnist = get_colored_mnist(mnist)
     return mnist.transpose(0, 3, 1, 2), colored_mnist
 
+
 def visualize_cyclegan_datasets():
     mnist, colored_mnist = load_q4_data()
     mnist, colored_mnist = mnist[:100], colored_mnist[:100]
-    show_samples(mnist.reshape([100, 28, 28, 1]) * 255.0, title=f'MNIST samples')
-    show_samples(colored_mnist.transpose([0, 2, 3, 1]) * 255.0, title=f'Colored MNIST samples')
+    show_samples(mnist.reshape([100, 28, 28, 1]) * 255.0, title=f"MNIST samples")
+    show_samples(
+        colored_mnist.transpose([0, 2, 3, 1]) * 255.0, title=f"Colored MNIST samples"
+    )
+
 
 def q4_save_results(fn):
     mnist, cmnist = load_q4_data()
@@ -212,11 +286,16 @@ def q4_save_results(fn):
     mnist_reconstructions = np.concatenate([m1, c1, m2], axis=0)
     colored_mnist_reconstructions = np.concatenate([c2, m3, c3], axis=0)
 
-    show_samples(mnist_reconstructions * 255.0, nrow=20,
-                 fname='figures/q4_mnist.png',
-                 title=f'Source domain: MNIST')
-    show_samples(colored_mnist_reconstructions * 255.0, nrow=20,
-                 fname='figures/q4_colored_mnist.png',
-                 title=f'Source domain: Colored MNIST')
+    show_samples(
+        mnist_reconstructions * 255.0,
+        nrow=20,
+        fname="figures/q4_mnist.png",
+        title=f"Source domain: MNIST",
+    )
+    show_samples(
+        colored_mnist_reconstructions * 255.0,
+        nrow=20,
+        fname="figures/q4_colored_mnist.png",
+        title=f"Source domain: Colored MNIST",
+    )
     pass
-
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='deepul',
-    version='0.1.0',
+    name="deepul",
+    version="0.1.0",
     packages=find_packages(),
-    license='MIT License',
+    license="MIT License",
 )
